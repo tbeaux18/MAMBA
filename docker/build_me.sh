968c71a06439142853b93115b009b3e2cd5bea40
tbaker18 <tim.baker@tempus.com>
2019-09-25 20:42:46 -0500

    made more updates

--- docker/build_me.sh
+++ docker/build_me.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker build -f Dockerfile.ubuntubase -t ubuntu1804:base .
-docker build -f Dockerfile.rbase -t ubuntu:r35 .
+docker build -f Dockerfile.rbase -t ubuntu:rbase35 .
 docker build -f Dockerfile.fastqc -t ubuntu:fastqc .
